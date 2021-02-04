0387d56e37b5c9644b911400bf3647e127a0abec
cocoa-coding <coco938178@gmail.com>
2021-02-04 16:53:23 +0800

    Vue.js

--- js/all.js
+++ js/all.js
@@ -1,121 +1,46 @@
-var xhr = new XMLHttpRequest();
-xhr.open('get' , 'https://raw.githubusercontent.com/hexschool/KCGTravel/master/datastore_search.json' ,true);
-xhr.send(null);
+new Vue({
+  el: '#vm',
+  data: {
+    kaohsiungData: null,
+    allarea: [],
+    hotArea: ['左營區','三民區','岡山區','甲仙區'],
 
-//DOM
-var allArea = document.querySelector('.allArea');
-var options = document.querySelector('.options');
-var hotArea = document.querySelector('.hotArea .clearfix');
-var area = document.querySelector('.areaContent h4');
+    currentArea: '',
+    allItem: true,
+  },
+  created(){
+    this.fetchAPI();
+  },
+  methods: {
+    fetchAPI() {
+      const xhr = new XMLHttpRequest();
+      xhr.open('get' , 'https://raw.githubusercontent.com/hexschool/KCGTravel/master/datastore_search.json' , true);
+      xhr.send(null);
 
-//addEventListener
-allArea.addEventListener('click' , showList , false);
-document.addEventListener('click' , hideList , false);
-options.addEventListener('click' , hideListAndClickArea , false);
-hotArea.addEventListener('click' , clickArea , false);
+      const self = this;
+      xhr.onload = function() {
+        self.kaohsiungData = JSON.parse(xhr.responseText).result.records;
 
-//function
-var data;
-xhr.onload = function() {
-  data = JSON.parse(xhr.responseText).result.records;
-  webpageRefresh();
-}
-
-function webpageRefresh(){
-  emptyContent();
-  for (var i = 0 ; i < data.length ; i++) {
-    updataPage(data[i]);
+        let areName = [];
+        for(let i = 0 ; self.kaohsiungData.length > i ; i++){
+          areName.push(self.kaohsiungData[i].Zone);
+        }
+        self.allarea = [...(new Set(areName))];
+      }
+    },
+    showList(){
+      this.$el.querySelector('.options').style.display = "block";
+    },
+    hideList() {
+      this.$el.querySelector('.options').style.display = "none";
+    },
+    clickArea(event) {
+      let eTarget = event.target.textContent;
+      this.$el.querySelector('.areaContent h4').textContent = eTarget;
+      this.$el.querySelector('.choiceArea ul>li>a').textContent = eTarget;
+      this.currentArea = eTarget;
+      this.allItem = false;
+      this.hideList();
+    },
   }
-}
-
-function showList(e) {
-  e.stopPropagation();
-  options.style.display = "block";
-}
-
-function hideList(e) {
-  e.stopPropagation();
-  options.style.display = "none";
-}
-
-
-function hideListAndClickArea(e){
-  hideList(e)
-  clickArea(e)
-}
-
-function clickArea(e){
-  if(e.target.nodeName !== "A"){return};
-  e.preventDefault();
-  emptyContent()
-  area.textContent = e.target.textContent;
-
-  for (var i = 0 ; i < data.length ; i++) {
-    var dataZone = data[i].Zone;
-    if(dataZone == e.target.textContent){
-      updataPage(data[i]);
-    }
-  }
-}
-
-function emptyContent(){
-  document.querySelector('.row').innerHTML = "";
-}
-
-function updataPage(index){
-  var dataPicture = index.Picture1;
-  var dataName = index.Name;
-  var dataZone = index.Zone;
-  var dataOpentime = index.Opentime;
-  var dataAdd = index.Add;
-  var dataTel = index.Tel;
-  var dataTicketinfo = index.Ticketinfo;
-
-  var colDOM = document.createElement('div')
-  colDOM.setAttribute('class' , 'col-sm-6');
-
-  var areaImgDOM = document.createElement('div')
-  areaImgDOM.setAttribute('class' , 'areaImg');
-  areaImgDOM.style.backgroundImage = 'url('+dataPicture+')';
-
-  var areaInfoDOM = document.createElement('div')
-  areaInfoDOM.setAttribute('class' , 'areaInfo');
-
-  colDOM.appendChild(areaImgDOM);
-  colDOM.appendChild(areaInfoDOM);
-
-  //areaImgDOM
-  var areaImgDOM1 = document.createElement('p');
-  areaImgDOM1.setAttribute('class' , 'attraction');
-  areaImgDOM1.textContent = dataName;
-
-  var areaImgDOM2 = document.createElement('p') ;
-  areaImgDOM2.setAttribute('class' , 'area');
-  areaImgDOM2.textContent = dataZone;
-
-  areaImgDOM.appendChild(areaImgDOM1);
-  areaImgDOM.appendChild(areaImgDOM2);
-
-  //areaInfoDOM
-  var areaInfoDOM1 = createAreaInfoDOM(dataOpentime, 'openTime', 'images/icons_clock.png')
-  var areaInfoDOM2 = createAreaInfoDOM(dataAdd, 'address', 'images/icons_pin.png')
-  var areaInfoDOM3 = createAreaInfoDOM(dataTel, 'tel', 'images/icons_phone.png')
-  var areaInfoDOM4 = createAreaInfoDOM(dataTicketinfo, 'ticket', 'images/icons_tag.png')
-  
-  areaInfoDOM.appendChild(areaInfoDOM1);
-  areaInfoDOM.appendChild(areaInfoDOM2);
-  areaInfoDOM.appendChild(areaInfoDOM3);
-  areaInfoDOM.appendChild(areaInfoDOM4); 
-
-  document.querySelector('.row').appendChild(colDOM);
- }
-
- function createAreaInfoDOM(textContent, className, imageSrc) {
-  var areaInfoDOM = document.createElement('p');
-  areaInfoDOM.setAttribute('class' , className);
-  areaInfoDOM.textContent = textContent;
-  var areaInfoDOMImg = document.createElement('img')
-  areaInfoDOMImg.setAttribute('src' , imageSrc);
-  areaInfoDOM.prepend(areaInfoDOMImg);
-  return areaInfoDOM
-}
+})  //end of vm
